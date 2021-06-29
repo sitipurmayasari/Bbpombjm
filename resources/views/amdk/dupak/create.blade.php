5a80c652206124faa267f0da92bf7fbb10a54dce
maya <sitipurmayasari89@gmail.com>
2021-06-29 10:42:44 +0800

    koma

--- resources/views/amdk/dupak/create.blade.php
+++ resources/views/amdk/dupak/create.blade.php
@@ -222,7 +222,7 @@
                         for="form-field-1">Start Poin
                         </label>
                         <div class="col-sm-9" >
-                            <input type="number" min="0" class="col-xs-1 col-sm-1 required "  placeholder="0" onkeyup="reskredit()"
+                            <input type="number" step="0.01" min="0" class="col-xs-1 col-sm-1 required "  placeholder="0" onkeyup="reskredit()"
                             name="startpoin" id="darinol"/>&nbsp;
                         </div>
                     </div>
