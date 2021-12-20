c2b4e482b60170b9097ba3e2cba2766108fbf8a5
Maya <sitipurmayasari89@gmail.com>
2021-12-20 08:52:48 +0800

    disetujui

--- resources/views/invent/carrent/edit.blade.php
+++ resources/views/invent/carrent/edit.blade.php
@@ -10,33 +10,90 @@
     <title>Surat Persetujuan Peminjaman kendaraan dinas</title>
 </head>
 <style>
-    html, table{
+    html{
         font-family: "Bookman Old Style";
-        font-size: 12;
+        font-size: 11;
+        margin-left: 3%;
+        margin-right: 30%;
     }
 
     #kepala{
         font-weight: bold;
         text-align: center;
     }
+
+    #isi{
+        font-family: "Bookman Old Style";
+        font-size: 10;
+        margin-left: 10%;
+        line-height: 1;
+        text-align: justify;
+    }
+
+    .demo-wrap {
+    overflow: hidden;
+    position: relative;
+    }
+
+    .demo-bg {
+    opacity: 0.3;
+    position: absolute;
+    left: 30%;
+    top: 10%;
+    width: 30%;
+    height: auto;
+    }
+
+    .demo-content {
+    position: relative;
+    
+    }
         
 
 </style>
-<body>
+<body class="demo-wrap">
+    <img class="demo-bg" src="{{asset('images/disetujui.png')}}" alt="" >
     <div id="kepala">
-        <h3>Tiket</h3>
-        <h1>PERSETUJUAN PEMINJAMAN KENDARAAN DINAS</h1>
+        <h4>Tiket</h4>
+        <h3>PERSETUJUAN PEMINJAMAN KENDARAAN DINAS</h3>
+        <br>
     </div>
-    <div>
-        <table>
-            <tr>
-                <td>Nama</td>
-                <td>: {{$data->pegawai->name}}</td>
-            </tr>
-            <tr>
-                <td></td>
-            </tr>
-        </table>
+    <div class="isi">
+        <div  class="demo-content">
+            <table>
+                <tr>
+                    <td style="vertical-align: top;">Nama</td>
+                    <td>:</td>
+                    <td> {{$data->pegawai->name}} ({{$data->pegawai->divisi->nama}})</td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top;">Tanggal Peminjaman</td>
+                    <td>:</td>
+                    <td> {{tgl_indo($data->date_from)}} s/d {{tgl_indo($data->date_to)}}</td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top;">Tujuan</td>
+                    <td>:</td>
+                    <td> {{$data->destination}}</td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top;">Kendaraan</td>
+                    <td>:</td>
+                    <td> {{$data->car->merk}} ({{$data->car->police_number}})</td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top;">Supir</td>
+                    <td>:</td>
+                    <td>
+                        @if ( $data->driver_id != null)
+                            {{$data->supir->name}}
+                        @else
+                            {{ ' - ' }}
+                        @endif
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
 </body>
 </html>
