8e8d143cf515a40b45fcfb5f627b5e45f19c3345
Maya <sitipurmayasari89@gmail.com>
2022-03-16 10:39:02 +0800

    pag break not working

--- resources/views/finance/travelexpenses/receipt.blade.php
+++ resources/views/finance/travelexpenses/receipt.blade.php
@@ -77,56 +77,58 @@
     
 <body>
 
-    <table style="width: 100%; padding" class="kepala">
-        <tr>
-            <td style="vertical-align: bottom; text-align: center;"  colspan="2">
-                <img src="{{asset('images/BBRI.jpg')}}" style="height:50px">
-            </td>
-            <td colspan="2" style=" font-size: 6;">
-                Lampiran VI (4 dari 4) <br>
-                Peraturan Menteri Keuangan tentang Perjalanan Dinas <br>
-                Dalam Negeri bagi Pejabat Negara Pegawai Negeri Sipil <br>
-                Dan Pegawai Tidak Tetap <br>
-                Nomor 113/PMK.05/2012 &nbsp;&nbsp;&nbsp; Tgl. 23 Juli 2012
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2" style="text-align: center;  font-size: 10; font-style: normal;">BALAI BESAR PENGAWAS OBAT DAN MAKANAN</td>
-            <td style="width: 18%">Akun</td>
-            <td style="width: 25%">: {{$item->out->pok->sub->komponen->code}} / {{$item->out->pok->akun->code}} )</td>
-        </tr>
-        <tr>
-            <td colspan="2" style="text-align: center;  font-size: 10;" > Di Banjarmasin</td>
-            <td>Bukti Kas</td>
-            <td>: ............................</td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <div id="border">
-                    KWITANSI
-                </div>
-            </td>
-            <td>Tahun Anggaran</td>
-            <td>: {{$item->out->pok->pok->year}}</td>
-        </tr>
-        <tr>
-            <td style="width: 20%" >Sudah Terima dari</td>
-            <td colspan="3">: Pejabat Pembuat komitmen Balai Besar Pengawas Obat dan Makanan di Banjarmasin</td>
-        </tr>
-        <tr>
-            <td>Uang Sebesar</td>
-            <td colspan="3">: <b>Rp.  &nbsp;&nbsp;&nbsp; 
-                @php
-                    $total = $injectQuery->totalHarga($item->id)
-                @endphp
-                {{number_format($total)}} 
-            </b></td>
-        </tr>
-        <tr>
-            <td>Untuk Pembayaran</td>
-            <td colspan="3">: <b>{{$data->st->purpose}}</b></td>
-        </tr>
-   </table>
+    <div style="page-break-before: always;">
+        <table style="width: 100%; padding" class="kepala">
+            <tr>
+                <td style="vertical-align: bottom; text-align: center;"  colspan="2">
+                    <img src="{{asset('images/BBRI.jpg')}}" style="height:50px">
+                </td>
+                <td colspan="2" style=" font-size: 6;">
+                    Lampiran VI (4 dari 4) <br>
+                    Peraturan Menteri Keuangan tentang Perjalanan Dinas <br>
+                    Dalam Negeri bagi Pejabat Negara Pegawai Negeri Sipil <br>
+                    Dan Pegawai Tidak Tetap <br>
+                    Nomor 113/PMK.05/2012 &nbsp;&nbsp;&nbsp; Tgl. 23 Juli 2012
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: center;  font-size: 10; font-style: normal;">BALAI BESAR PENGAWAS OBAT DAN MAKANAN</td>
+                <td style="width: 18%">Akun</td>
+                <td style="width: 25%">: {{$item->out->pok->sub->komponen->code}} / {{$item->out->pok->akun->code}} )</td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: center;  font-size: 10;" > Di Banjarmasin</td>
+                <td>Bukti Kas</td>
+                <td>: ............................</td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <div id="border">
+                        KWITANSI
+                    </div>
+                </td>
+                <td>Tahun Anggaran</td>
+                <td>: {{$item->out->pok->pok->year}}</td>
+            </tr>
+            <tr>
+                <td style="width: 20%" >Sudah Terima dari</td>
+                <td colspan="3">: Pejabat Pembuat komitmen Balai Besar Pengawas Obat dan Makanan di Banjarmasin</td>
+            </tr>
+            <tr>
+                <td>Uang Sebesar</td>
+                <td colspan="3">: <b>Rp.  &nbsp;&nbsp;&nbsp; 
+                    @php
+                        $total = $injectQuery->totalHarga($item->id)
+                    @endphp
+                    {{number_format($total)}} 
+                </b></td>
+            </tr>
+            <tr>
+                <td>Untuk Pembayaran</td>
+                <td colspan="3">: <b>{{$data->st->purpose}}</b></td>
+            </tr>
+       </table>
+    </div>
    <br>
    <table style="width: 100%; padding" class="kepala">
         <tr>
@@ -1174,7 +1176,8 @@
    </table>
     <hr style="border:0.5px solid black; margin: 7px;">
    <p style="font-size: 10; text-align:center;"><b>PERHITUNGAN SPPD RAMPUNG</b></p>
-   <table style="width: 100%;" class="kepala">
+   <div style="page-break-after: always;">
+    <table style="width: 100%;" class="kepala">
         <tr>
             <td style="width: 20%"></td>
             <td style="width: 23%">Ditetapkan sejumlah</td>
@@ -1216,6 +1219,7 @@
             </td>
         </tr>
    </table>
+   </div>
 </body>
 
 @endforeach
@@ -1226,8 +1230,6 @@
    
     var total = document.getElementById("total_terbilang").value;
     document.getElementById("uangSebesar").innerHTML = total;
-
-
 </script>
 @endsection
 
