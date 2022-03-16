30bd636a6c5d8f6d8f4809f2bdd7ce9c179b39c1
Maya <sitipurmayasari89@gmail.com>
2022-03-16 08:47:03 +0800

    tr

--- app/Http/Controllers/Finance/TravelexpensesController.php
+++ app/Http/Controllers/Finance/TravelexpensesController.php
@@ -540,9 +540,9 @@ class TravelexpensesController extends Controller
         } else {
             // return view('finance/travelexpenses.receipt',compact('petugas','data','pegawai','tujuan'));
            if ($jmltujuan->hitung == 1) {
-                // $pdf        = PDF::loadview('finance/travelexpenses.receipt',compact('petugas','data','pegawai','tujuan'));
-                // return $pdf->stream();
-                return view('finance/travelexpenses.receipt',compact('petugas','data','pegawai','tujuan'));
+                $pdf        = PDF::loadview('finance/travelexpenses.receipt',compact('petugas','data','pegawai','tujuan'));
+                return $pdf->stream();
+                // return view('finance/travelexpenses.receipt',compact('petugas','data','pegawai','tujuan'));
            } else {
                 $pdf        = PDF::loadview('finance/travelexpenses.receiptpdf',compact('petugas','data','pegawai','tujuan'));
                 return $pdf->stream();
