09e63a6b5b9d7580b5bfdcbfdf04465e74b0118b
Maya <sitipurmayasari89@gmail.com>
2022-07-20 09:03:03 +0800

    napza2

---
+++ resources/views/calibration/comingsoon.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.lab')
+@section('content')
+<style>
+    .sidebar{
+        overflow: auto;
+        
+    }
+</style>
+<div>
+  <img src="{{asset('images/coomingsoon.png')}}" style="width: 100%;">
+</div>
+
+@endsection
---
+++ resources/views/calibration/dashboardnapza/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.lab')
+@section('breadcrumb')
+    <li><i class="fa fa-tachometer"> Dashboard</i></li>
+@endsection
+@section('content')
+<link href="{{asset('assets/css/material.css')}}" rel="stylesheet">
+<link href="{{asset('assets/css/demo.css')}}" rel="stylesheet" /> 
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
+<div class="row">
+
+</div>
+@endsection
+@section('footer')
+@endsection
