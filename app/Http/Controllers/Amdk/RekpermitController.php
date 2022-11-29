71da4de0bcc2f0d78dfbbcd77fc418f87921c2fe
Maya <sitipurmayasari89@gmail.com>
2022-11-29 10:46:09 +0800

    ok

--- app/Http/Controllers/Amdk/RekpermitController.php
+++ app/Http/Controllers/Amdk/RekpermitController.php
@@ -20,7 +20,7 @@ class RekpermitController extends Controller
 {
     public function index(Request $request)
     {
-        $data = Absensi::orderBy('id','desc')->orderBy('users_id','asc')
+        $data = Absensi::orderBy('tanggal','desc')
                         ->SelectRaw('absensi.*, users.name,
                                     CASE
                                         WHEN periode_month = 1 THEN "Januari"
