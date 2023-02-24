2738909a699fe50e3f1945e48b1e27d0a8b7e2bc
Maya <sitipurmayasari89@gmail.com>
2023-02-24 13:27:24 +0800

    st

--- app/Http/Controllers/Finance/OutstationController.php
+++ app/Http/Controllers/Finance/OutstationController.php
@@ -233,14 +233,40 @@ class OutstationController extends Controller
                   ->where('outstation_id','=',$id)
                   ->first();
         }
-                            
+            
         if ($data->type=='DL') {
           $pdf = PDF::loadview('finance/outstation.inside',compact('data','isian','destinys','lama'));
         } elseif ($data->type=='DL8') {
             $pdf = PDF::loadview('finance/outstation.inside2',compact('data','isian','destinys','lama'));
         } else {
-          $pdf = PDF::loadview('finance/outstation.sppdnewN',compact('data','isian','menyetujui','destinys','lama'));
+          if ($data->external=='N') {
+            if ($hit->jum==3) {
+              $pdf = PDF::loadview('finance/outstation.printSppd3',compact('data','isian','menyetujui','destinys','lama'));
+            }else{
+              $pdf = PDF::loadview('finance/outstation.printSppd',compact('data','isian','menyetujui','destinys','lama'));
+            }
+          } else if ($data->external=='L') {
+            if ($hit->jum==3) {
+              $pdf = PDF::loadview('finance/outstation.printSppd3la',compact('data','isian','menyetujui','destinys','lama'));
+            }else{
+              $pdf = PDF::loadview('finance/outstation.printSppdla',compact('data','isian','menyetujui','destinys','lama'));
+            }
+          } else {
+            if ($hit->jum==3) {
+              $pdf = PDF::loadview('finance/outstation.printSppd3ex',compact('data','isian','menyetujui','destinys','lama'));
+            }else{
+              $pdf = PDF::loadview('finance/outstation.printSppdex',compact('data','isian','menyetujui','destinys','lama'));
+            }
+          }
+          
         }
+        // if ($data->type=='DL') {
+        //   $pdf = PDF::loadview('finance/outstation.inside',compact('data','isian','destinys','lama'));
+        // } elseif ($data->type=='DL8') {
+        //     $pdf = PDF::loadview('finance/outstation.inside2',compact('data','isian','destinys','lama'));
+        // } else {
+        //   $pdf = PDF::loadview('finance/outstation.sppdnewN',compact('data','isian','menyetujui','destinys','lama'));
+        // }
         
 
         return $pdf->stream();
