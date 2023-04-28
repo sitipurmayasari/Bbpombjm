362329fd364e4538bf1d973b5f22eee6e7c8f7f8
Maya <sitipurmayasari89@gmail.com>
2023-04-28 13:42:26 +0800

    search

--- app/Http/Controllers/Amdk/LogActController.php
+++ app/Http/Controllers/Amdk/LogActController.php
@@ -13,13 +13,11 @@ class LogActController extends Controller
     public function index(Request $request)
     {
         $data = LogActivity::orderBy('id','desc')
-                            ->SelectRaw('log_activities.*')
+                            ->SelectRaw('log_activities.*, users.name')
                             ->leftjoin('users','users.id','log_activities.users_id')
                             ->when($request->keyword, function ($query) use ($request) {
                                 $query->where('subject','LIKE','%'.$request->keyword.'%')
-                                        ->orWhere('users.name', 'LIKE','%'.$request->keyword.'%')
-                                        ->orWhere('ip', 'LIKE','%'.$request->keyword.'%')
-                                        ->orWhere('kelompok', 'LIKE','%'.$request->keyword.'%');
+                                        ->orWhere('users.name', 'LIKE','%'.$request->keyword.'%');
                                 })
                                 ->paginate('10');
         return view('amdk/logact.index',compact('data'));
