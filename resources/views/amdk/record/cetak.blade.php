ee12ea7464d9b63e6ad7c05e11a217262aea935b
Maya <sitipurmayasari89@gmail.com>
2023-05-24 09:50:44 +0800

    printexcel

--- resources/views/amdk/record/cetak.blade.php
+++ resources/views/amdk/record/cetak.blade.php
@@ -1,6 +1,6 @@
 <?php
-// header("Content-type: application/vnd-ms-excel");
-// header("Content-Disposition: attachment; filename=Rekam-personil-pns.xls");
+header("Content-type: application/vnd-ms-excel");
+header("Content-Disposition: attachment; filename=Rekam-personil-pns.xls");
 ?>
 
 <!DOCTYPE html>
