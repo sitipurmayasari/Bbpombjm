bf95b640187f4acb45febefc3f5db8a618e3e170
Maya <sitipurmayasari89@gmail.com>
2024-01-03 10:21:15 +0800

    NonAgrr

--- resources/views/finance/receipt/index.blade.php
+++ resources/views/finance/receipt/index.blade.php
@@ -51,33 +51,27 @@
                     <td>{{$row->date}}</td>
                     <td>{{$row->st->number}}</td>
                     <td>{{$row->st->purpose}}</td>
-                    <td style="text-align: center">
-                        @if ($row->st->pok_detail_id == 1 || $row->st->pok_detail_id == 0)
-                            <a class="btn btn-danger agr" href="#"
-                            rel="noopener noreferrer">NA</a>
-                        @else
-                        <a class="btn btn-primary agr" href="/finance/receipt/cost/{{$row->id}}" target="_blank" 
-                            rel="noopener noreferrer">Kuitansi</a>
-                        <a class="btn btn-warning agr" href="/finance/receipt/nominatif/{{$row->id}}" target="_blank" 
-                                rel="noopener noreferrer">Nominatif</a>
-                        @endif
-                    </td>
-                    <td style="text-align: center">
-                        @if ($row->st->pok_detail_id == 1 || $row->st->pok_detail_id == 0)
-                            <a class="btn btn-danger agr" href="#"
-                            rel="noopener noreferrer">NA</a>
-                        @else
+                    @if ($row->st->pok_detail_id == 1 || $row->st->pok_detail_id == 0)
+                        <td colspan="3" style="text-align: center">
+                            <a class="btn btn-danger agr" href="/finance/outstation/edit/{{$row->id}}"
+                            rel="noopener noreferrer">Non Anggaran</a>
+                            <br>
+                            *Silahkan Cek Kembali Surat Tugas Anda 
+                        </td>
+                    @else
+                        <td style="text-align: center">
+                            <a class="btn btn-primary agr" href="/finance/receipt/cost/{{$row->id}}" target="_blank" 
+                                rel="noopener noreferrer">Kuitansi</a>
+                            <a class="btn btn-warning agr" href="/finance/receipt/nominatif/{{$row->id}}" target="_blank" 
+                                    rel="noopener noreferrer">Nominatif</a>
+                        </td>
+                        <td style="text-align: center">
                             <a class="btn btn-primary agr" href="/finance/receipt/riil/{{$row->id}}" target="_blank" 
                                 rel="noopener noreferrer">Riil</a>
                             <a class="btn btn-warning agr" href="/finance/receipt/riilkkp/{{$row->id}}" target="_blank" 
                                 rel="noopener noreferrer">KKP</a>
-                        @endif
-                    </td>
-                    <td style="text-align: center">
-                        @if ($row->st->pok_detail_id == 1 || $row->st->pok_detail_id == 0)
-                            <a class="btn btn-danger agr" href="#"
-                            rel="noopener noreferrer">NA</a>
-                        @else
+                        </td>
+                        <td style="text-align: center">
                             <a class="btn btn-primary agr" href="/finance/receipt/super/{{$row->id}}" target="_blank" 
                                 rel="noopener noreferrer">SuPer</a> <br>
                             <a class="btn btn-warning agr" href="/finance/receipt/super30/{{$row->id}}" target="_blank" 
@@ -86,8 +80,8 @@
                                 <a class="btn btn-success agr" href="/finance/travelexpenses/super8J/{{$row->id}}" target="_blank" 
                                     rel="noopener noreferrer">SuPer>8Jam</a>
                             @endif
-                        @endif
-                    </td>
+                        </td>
+                    @endif
                     <td>
                         <a href="/finance/receipt/edit/{{$row->id}}" class="btn btn-warning">
                             <i class="glyphicon glyphicon-edit"></i>
