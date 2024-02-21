a8f066100b0341332cc76e790ef2feaeb4f2e3da
Maya <sitipurmayasari89@gmail.com>
2024-02-21 14:54:18 +0800

    kodebarang

--- resources/views/invent/labrequest/print2.blade.php
+++ resources/views/invent/labrequest/print2.blade.php
@@ -115,6 +115,7 @@
         <thead>
             <tr>
                 <th>NO</th>
+                <th>KODE BARANG</th>
                 <th>NAMA BARANG</th>
                 <th>SATUAN</th>
                 <th>JUMLAH</th>
@@ -128,6 +129,7 @@
             @foreach ($isi as $item)
             <tr>
                 <td>{{$no++}}</td>
+                <td>{{$item->barang->kode_barang}}</td>
                 <td style="text-align: left;">{{$item->barang->nama_barang}}</td>
                 <td>{{$item->satuan->satuan}}</td>
                 <td>{{$item->jumlah_aju}}</td>
@@ -265,6 +267,7 @@
         <thead>
             <tr>
                 <th>NO</th>
+                <th>KODE BARANG</th>
                 <th>NAMA BARANG</th>
                 <th>SATUAN</th>
                 <th>JUMLAH</th>
@@ -278,6 +281,7 @@
             @foreach ($isi as $item)
             <tr>
                 <td>{{$no++}}</td>
+                <td>{{$item->barang->kode_barang}}</td>
                 <td style="text-align: left;">{{$item->barang->nama_barang}}</td>
                 <td>{{$item->satuan->satuan}}</td>
                 <td>{{$item->jumlah}}</td>
