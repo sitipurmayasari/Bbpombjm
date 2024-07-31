a330b51e328cf97752414d0b0e54db50f5682ad0
Maya <sitipurmayasari89@gmail.com>
2024-07-31 08:05:24 +0800

    amdk

--- resources/views/amdk/dashboard/index.blade.php
+++ resources/views/amdk/dashboard/index.blade.php
@@ -23,7 +23,45 @@
       text-align: center;
     }
     
-</style>    
+</style> 
+<div class="col-sm-6">
+  <div class="col-sm-12" style="text-align: center">
+    <div class="card">
+        <div class="card-header card-header-warning">
+          <h4 class="card-title">TEMPLATE SURAT</h4>
+        </div>
+        <div class="card-body">
+          <table class="table table-hover" style="font-size: 12px;">
+            <tr>
+              <td style="text-align: left;">FORM ABSEN MANUAL</td>
+              <td>
+                <a href="https://docs.google.com/document/d/14PKmfPcp1-YDZSaFTwTw1EO4vXz2FIjA/edit?usp=sharing&ouid=106984688255716269129&rtpof=true&sd=true" target="_blank">LINK</a>
+              </td>
+             
+            </tr>
+            <tr>
+              <td style="text-align: left;">FORM LUPA ABSEN</td>
+              <td>
+                <a href="https://docs.google.com/document/d/1p46Ul-tYGAwCt7W7dis3jVtPdQLNeq1v/edit?usp=sharing&ouid=106984688255716269129&rtpof=true&sd=true" target="_blank">LINK</a>
+              </td>
+            </tr>
+            <tr>
+              <td style="text-align: left;">IZIN TIDAK MASUK KERJA (POTONG 5%/HARI)</td>
+              <td>
+                <a href="https://docs.google.com/document/d/1LT1IZvwnNkeR5wCwVdQxhB5vOcowHNLW/edit?usp=sharing&ouid=106984688255716269129&rtpof=true&sd=true" target="_blank">LINK</a>
+              </td>
+            </tr>
+            <tr>
+              <td style="text-align: left;">IZIN TERLAMBAT MASUK DAN PULANG SEBELUM WAKTUNYA</td>
+              <td>
+                <a href="https://docs.google.com/document/d/1af_hDFaW0YEOEikTCLBW92eG51wa0GcF/edit?usp=sharing&ouid=106984688255716269129&rtpof=true&sd=true" target="_blank">LINK</a>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+  </div>
+</div>   
 <div class="col-sm-3">
     <div class="card card-stats">
         @if ($latih->waktu <= 10 )
