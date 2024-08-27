662e0cc125a6b070c5e7746a83982d19412bf3e2
Maya <sitipurmayasari89@gmail.com>
2024-08-27 14:37:32 +0800

    row

--- resources/views/arsip/disposisi/index.blade.php
+++ resources/views/arsip/disposisi/index.blade.php
@@ -60,7 +60,7 @@
                         @endif
                     </td>
                     <td>
-                        <a href="{{$data->getfileDispo()}}" target="_blank" >{{$data->file}}</a>
+                        <a href="{{$row->getfileDispo()}}" target="_blank" >{{$data->file}}</a>
                     </td>
                     <td>
                         <a href="/arsip/disposisi/edit/{{$row->id}}" class="btn btn-warning">
