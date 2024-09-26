8061a4cab9fcf45707674b6d046523a0f030e16d
Maya <sitipurmayasari89@gmail.com>
2024-09-26 08:25:44 +0800

    IMPORTPEL

--- app/Imports/PelatihanImport.php
+++ app/Imports/PelatihanImport.php
@@ -23,7 +23,7 @@ class PelatihanImport implements ToModel,WithStartRow
     {
         $nip = str_replace(' ', '', $row[2]);
         $user = User::where('no_pegawai',$nip)->first();
-        // $user = User::where('no_pegawai',$row[2])->first();
+        $namapel = $row[7];
         $desk = "Nomor Sertifikat : ".$row[14]."( Tgl :".$row[15].")";
         $rekam = "Y";
         $dari = str_replace('/', '-', $row[12]);
@@ -38,20 +38,27 @@ class PelatihanImport implements ToModel,WithStartRow
              $eva = "N";
         }
         
+        $cek = Pelatihan::where('users_id', $user->id)
+                        ->where('nama', $namapel)
+                        ->where('dari',$tgldari)
+                        ->first();
 
         if ($user) {
-            return new Pelatihan([
-                'users_id'          => $user->id,
-                'jenis_pelatihan_id'=> $jenis,
-                'nama'              => $row[7],
-                'dari'              => $tgldari,
-                'sampai'            => $tglsampai,
-                'lama'              => $row[16],
-                'penyelenggara'     => $row[9],
-                'terekam'           => $rekam,
-                'deskripsi'         => $desk,
-                'evaluasi'          => $eva
-            ]);
+            if ($cek == null) {
+                return new Pelatihan([
+                    'users_id'          => $user->id,
+                    'jenis_pelatihan_id'=> $jenis,
+                    'nama'              => $row[7],
+                    'dari'              => $tgldari,
+                    'sampai'            => $tglsampai,
+                    'lama'              => $row[16],
+                    'penyelenggara'     => $row[9],
+                    'terekam'           => $rekam,
+                    'deskripsi'         => $desk,
+                    'evaluasi'          => $eva
+                ]);
+            }
+            
         }
     }
 }
